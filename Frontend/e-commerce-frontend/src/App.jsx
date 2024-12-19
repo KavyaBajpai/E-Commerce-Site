bc297df035e0b6f082b9311555d3f4ceec5f35bd
KavyaBajpai <kavyabajpai05@gmail.com>
2024-12-19 22:11:48 +0530

    added routes to all pages

--- Frontend/e-commerce-frontend/src/App.jsx
+++ Frontend/e-commerce-frontend/src/App.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Card from './pages/Card'
+import Collections from './pages/Collections'
+import Contact from './pages/Contact'
+import Orders from './pages/Orders'
+import PlaceOrder from './pages/PlaceOrder'
+import Product from './pages/Product'
+import Login from './pages/Login'
+
 
 const App = () => {
   return (
-    <div>
-      <h1>This is the home page of the website!</h1>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+        <Routes>
+           <Route path='/' element={<Home/>} />
+           <Route path='/collection' element={<Collections/>} />
+           <Route path='/about' element={<About/>} />
+           <Route path='/card' element={<Card/>} />
+           <Route path='/contact' element={<Contact/>} />
+           <Route path='/login' element={<Login/>} />
+           <Route path='/orders' element={<Orders/>} />
+           <Route path='/placeOrder' element={<PlaceOrder/>} />
+           <Route path='/product' element={<Product/>} />
+        </Routes>
     </div>
   )
 }
