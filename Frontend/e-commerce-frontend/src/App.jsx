985890fb57b70f67def5a8b21fdabdddb27a58d6
KavyaBajpai <kavyabajpai05@gmail.com>
2024-12-23 23:39:09 +0530

    added navbar

--- Frontend/e-commerce-frontend/src/App.jsx
+++ Frontend/e-commerce-frontend/src/App.jsx
@@ -2,23 +2,25 @@ import React from 'react'
 import { Routes,Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
-import Card from './pages/Card'
+import Cart from './pages/Cart'
 import Collections from './pages/Collections'
 import Contact from './pages/Contact'
 import Orders from './pages/Orders'
 import PlaceOrder from './pages/PlaceOrder'
 import Product from './pages/Product'
 import Login from './pages/Login'
+import Navbar from './components/Navbar'
 
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+        <Navbar />
         <Routes>
            <Route path='/' element={<Home/>} />
            <Route path='/collection' element={<Collections/>} />
            <Route path='/about' element={<About/>} />
-           <Route path='/card' element={<Card/>} />
+           <Route path='/cart' element={<Cart/>} />
            <Route path='/contact' element={<Contact/>} />
            <Route path='/login' element={<Login/>} />
            <Route path='/orders' element={<Orders/>} />
