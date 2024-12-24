3869d5ae89680fdb84943b5e495b0145962005ed
KavyaBajpai <kavyabajpai05@gmail.com>
2024-12-25 00:09:01 +0530

    added Title component

--- Frontend/e-commerce-frontend/src/components/LatestCollection.jsx
+++ Frontend/e-commerce-frontend/src/components/LatestCollection.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import { useContext } from 'react';
+import { ShopContext } from '../context/ShopContext';
+import Title from './Title';
 const LatestCollection = () => {
-  return (
-    <div>
+  
+   const {products} = useContext(ShopContext);
+   
+    return (
+    <div className='my-10'>
+        <div className='text-center py-8 text-3xl'>
+            <Title text1={'LATEST'} text2={' COLLECTIONS'}/>
+            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.</p>
+
+            
+        </div>
       
     </div>
   )
