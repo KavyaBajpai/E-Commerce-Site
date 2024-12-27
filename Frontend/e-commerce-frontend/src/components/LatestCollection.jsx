cd10118fc01ea3924d23c70e2a63664bc5ed1428
KavyaBajpai <kavyabajpai05@gmail.com>
2024-12-27 23:10:48 +0530

    some changes

--- Frontend/e-commerce-frontend/src/components/LatestCollection.jsx
+++ Frontend/e-commerce-frontend/src/components/LatestCollection.jsx
@@ -1,18 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useContext } from 'react';
 import { ShopContext } from '../context/ShopContext';
 import Title from './Title';
 const LatestCollection = () => {
   
    const {products} = useContext(ShopContext);
-   
+   const [latestCollection,setLatestProducts] = useState([]);
     return (
     <div className='my-10'>
         <div className='text-center py-8 text-3xl'>
             <Title text1={'LATEST'} text2={' COLLECTIONS'}/>
             <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.</p>
-
-            
         </div>
       
     </div>
