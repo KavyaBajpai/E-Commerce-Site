a5aca870f045f87ed4dfa70d349c5ac01d8bf02b
KavyaBajpai <kavyabajpai05@gmail.com>
2024-12-30 20:52:34 +0530

    added newsletter component

--- Frontend/e-commerce-frontend/src/pages/Home.jsx
+++ Frontend/e-commerce-frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import Bestseller from '../components/Bestseller'
 import OurPolicy from '../components/OurPolicy'
+import NewsletterBox from '../components/NewsletterBox'
 const Home = () => {
   return (
     <div>
@@ -10,6 +11,7 @@ const Home = () => {
        <LatestCollection/>
        <Bestseller/>
        <OurPolicy/>
+       <NewsletterBox/>
     </div>
   )
 }
