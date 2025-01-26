79c529f10c1d9dc9e6b5d85c3df3438970038a3b
KavyaBajpai <kavyabajpai05@gmail.com>
2025-01-26 23:52:41 +0530

    orders page

--- Frontend/e-commerce-frontend/src/pages/Orders.jsx
+++ Frontend/e-commerce-frontend/src/pages/Orders.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
 
 const Orders = () => {
+
+  const {products, currency} = useContext(ShopContext);
   return (
-    <div>
+    <div className='broder-t pt-5'>
       
     </div>
   )
