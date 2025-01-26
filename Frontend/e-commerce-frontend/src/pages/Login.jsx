71e5a4b8c862ed02ab5e4badeb838d14a4239851
KavyaBajpai <kavyabajpai05@gmail.com>
2025-01-27 01:27:11 +0530

    login page

--- Frontend/e-commerce-frontend/src/pages/Login.jsx
+++ Frontend/e-commerce-frontend/src/pages/Login.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Login = () => {
+
+  const [currState, setCurrState] = useState('Sign Up')
+
   return (
-    <div>
-      
-    </div>
+    <form className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800 '>
+      <div className='inline-flex gap-2 mb-2 mt-10 items-center'>
+        <p className='prata-regular text-3xl'>{currState}</p>
+        <hr className='border-none h-[1.5px] w-8 bg-gray-800'></hr>
+      </div>
+
+      {currState ==='Login' ? '' : <input type="text" className='w-full px-3 py-2 border border-gray-800' placeholder='Name' required></input>}
+      <input type="email" className='w-full px-3 py-2 border border-gray-800' placeholder='Email' required></input>
+      <input type="text" className='w-full px-3 py-2 border border-gray-800' placeholder='Password' required></input>
+      <div className='w-full flex justify-between text-sm mt-[-8px]'>
+      {currState ==='Login' ? <p className='cursor-pointer'>Forgot your password?</p> : ''}  
+      {currState ==='Login' ? <p onClick={()=>setCurrState('Sign Up')} className='cursor-pointer'>Create an account</p> : <p onClick={()=>setCurrState('Login')} className='cursor-pointer'>Already have an account?</p>}
+      </div>
+      <button className='bg-black text-white font-light px-8 py-2 mt-4'>{currState === 'Login' ? 'Sign In' : 'Sign Up'}</button>
+    </form>
   )
 }
 
