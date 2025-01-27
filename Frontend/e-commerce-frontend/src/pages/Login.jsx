853a1dd139e2be2cea5de0fba0bb679f42f0a6ca
KavyaBajpai <kavyabajpai05@gmail.com>
2025-01-27 11:17:58 +0530

    login page

--- Frontend/e-commerce-frontend/src/pages/Login.jsx
+++ Frontend/e-commerce-frontend/src/pages/Login.jsx
@@ -4,14 +4,17 @@ import { useState } from 'react'
 const Login = () => {
 
   const [currState, setCurrState] = useState('Sign Up')
+  const onSubmitHandler = async (e) => {
+    e.preventDefault()
+  }
 
   return (
-    <form className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800 '>
+    <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800 '>
       <div className='inline-flex gap-2 mb-2 mt-10 items-center'>
         <p className='prata-regular text-3xl'>{currState}</p>
         <hr className='border-none h-[1.5px] w-8 bg-gray-800'></hr>
       </div>
-
+      
       {currState ==='Login' ? '' : <input type="text" className='w-full px-3 py-2 border border-gray-800' placeholder='Name' required></input>}
       <input type="email" className='w-full px-3 py-2 border border-gray-800' placeholder='Email' required></input>
       <input type="text" className='w-full px-3 py-2 border border-gray-800' placeholder='Password' required></input>
