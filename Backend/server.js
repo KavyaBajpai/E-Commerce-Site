cd0c93de1851e6e3b420b8f52d5ff36d3bb280a3
KavyaBajpai <kavyabajpai05@gmail.com>
2025-01-28 13:03:57 +0530

    database connection done

---
+++ Backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+
+
+
+//App Config
+const app= express();
+const port = process.env.PORT || 4000;
+connectDB();
+
+//Middlewares
+app.use(express.json());
+app.use(cors());
+
+//API Endpoints
+app.get('/', (req, res)=>{
+   res.send("API Working")
+})
+//res.send is sending a response to the client onto the webpage... webpage pe client ke question ka answer likh ke de rha hai...
+
+app.listen(port, ()=> {
+    console.log(`Server is running on port ${port}`)
+})
