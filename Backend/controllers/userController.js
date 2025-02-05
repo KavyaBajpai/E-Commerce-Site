0751c43687f8b931339044e2263dc8a753995d62
KavyaBajpai <kavyabajpai05@gmail.com>
2025-02-06 00:31:59 +0530

    creating user routes

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,17 @@
+
+
+const loginUser = async (req, res) => {
+
+}
+
+const registerUser = async (req, res) => {
+   
+    res.json({msg: "register api working"})
+}
+
+const adminLogin = async (req, res) => {
+
+}
+
+
+export { loginUser, registerUser, adminLogin };
