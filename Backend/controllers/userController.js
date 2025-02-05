9840afb41e93306334e8835d945b698ffd130cab
KavyaBajpai <kavyabajpai05@gmail.com>
2025-02-06 00:47:26 +0530

    user controllers

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,4 +1,6 @@
-
+import userModel from "../models/userModel.js";
+import validator from 'validator';
+import bcrypt from 'bcryptjs';
 
 const loginUser = async (req, res) => {
 
@@ -6,7 +8,38 @@ const loginUser = async (req, res) => {
 
 const registerUser = async (req, res) => {
    
-    res.json({msg: "register api working"})
+    try {
+         const { name, email, password } = req.body;
+
+         const exists = await userModel.findOne({ email });
+            if (exists) {
+                return res.status(400).json({success:false, message: "User already exists" });
+            }
+
+            if (!validator.isEmail(email)) {
+                return res.status(400).json({success:false, message: "Invalid Email" });
+            }
+
+            if (password.length < 6) {
+                return res.status(400).json({success:false, message: "Enter strong password" });
+            }
+
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+
+            const newUser = new userModel({
+                name, email, password: hashedPassword
+            })
+
+            const user = await newUser.save();
+
+            const token = await 
+    }
+
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({success:false, message: "Internal Server Error" });
+    }
 }
 
 const adminLogin = async (req, res) => {
