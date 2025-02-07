16430af2bbcc39f490d6fb91e5f7edbbdc8180dd
KavyaBajpai <kavyabajpai05@gmail.com>
2025-02-07 17:18:38 +0530

    completed product controllers

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -46,15 +46,37 @@ const addProduct = async (req,res) => {
 }
 
 const listProducts = async (req, res ) => {
-
+    try {
+        const products = await productModel.find({});
+        res.json({success:true, products});
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({success:false, message: error.message });
+    }
 }
 
 const removeProducts = async (req, res ) => {
-    
+    try {
+        await productModel.findByIdAndDelete(req.body.id);
+        res.json({success:true, message: "Product Removed Successfully"});
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({success:false, message: error.message });
+    }
 }
 
 const singleProducts = async (req, res ) => {
-    
+    try{
+        const {productId} = req.body;
+        const product = await productModel.findById(productId);
+        res.json({success:true, product});
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({success:false, message: error.message });
+    }
 }
 
 
