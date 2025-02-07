1e658fc366ced664abcb90ba27b0ab60d6505f4b
KavyaBajpai <kavyabajpai05@gmail.com>
2025-02-07 18:23:48 +0530

    added admin authentication logic

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -72,7 +72,22 @@ const registerUser = async (req, res) => {
 }
 
 const adminLogin = async (req, res) => {
+    try{
+         const { email, password} = req.body
 
+         if( email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+             const token = jwt.sign(email+password, process.env.JWT_SECRET);   
+             return res.json({success:true, token });
+         }
+         else 
+         {
+            res.json({success: false, message: "Invalid Credentials" });
+         }
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({success:false, message: "Internal Server Error" });
+    }
 }
 
 
