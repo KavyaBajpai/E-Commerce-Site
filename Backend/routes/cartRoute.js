89c539330594031f24d99649bf7d1f26c255dc64
KavyaBajpai <kavyabajpai05@gmail.com>
2025-02-15 15:50:50 +0530

    adding cart router

---
+++ Backend/routes/cartRoute.js
@@ -0,0 +1,10 @@
+import { addToCart, updateCart, getUserCart } from "../controllers/cartController"; 
+import express from 'express';
+
+const cartRouter = express.Router()
+
+cartRouter.post('/get', getUserCart)
+cartRouter.post('/add', addToCart)
+cartRouter.post('/update', updateCart)
+
+export default cartRouter
